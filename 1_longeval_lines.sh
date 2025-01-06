b60f0fd2c67086a6e252e20fbaa7219ea0cde897
Qinglei Cao <qcao3@guyot.cluster>
2025-01-06 17:49:42 +0000

    Update script

--- 1_longeval_lines.sh
+++ 1_longeval_lines.sh
@@ -2,20 +2,22 @@
 
 model_names=(
     "HanzhiZhang_H2O-llama-model"
-    "HanzhiZhang_StreamingLLM-llama-model"
-    "HanzhiZhang_MoA-llama-model"
-    "HanzhiZhang_DAM_0.99"
-    "meta-llama_Llama-3.2-3B-Instruct"
+    #"HanzhiZhang_StreamingLLM-llama-model"
+    #"HanzhiZhang_MoA-llama-model"
+    #"HanzhiZhang_DAM_0.99"
+    #"meta-llama_Llama-3.2-3B-Instruct"
 )
 
 for model_name in "${model_names[@]}"; do
-    for ite in {1..10}; do
-        python longeval/eval.py \
+    #for ite in {1..10}; do
+    for ite in $1; do
+        python3.12 longeval/eval.py \
             --model-name-or-path "model/$model_name" \
             --count "$ite" \
             --task lines \
             --max_gpu_memory 80 \
-            --num_gpus 2 &
+            --test_dir "longeval/evaluation" \
+            --num_gpus 8 &
         wait
     done
 done
